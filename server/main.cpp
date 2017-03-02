73b583de6cb0cbd7fd77a1bfdeb7733ee803dd16
Dominik Madarasz <madaraszd@mail.ru>
2017-03-02 16:49:46 +0100

    Added small entity test

--- server/main.cpp
+++ server/main.cpp
@@ -1,7 +1,36 @@
 #include <librg/server.h>
+#include <librg/Core.h>
+
+struct CubeData
+{
+    int res;
+    float wallSize;
+    bool hollow;
+};
+
+using namespace Server;
+
+enum
+{
+    EntityType_Cube = EntityType_FirstFree,
+    // ...
+};
+
+entity_id create_cube(int res, float size, bool hollow) {
+    auto data = new CubeData {res, wallSize, hollow };
+
+    auto entity = Entity::Manager::Instance()->Create(EntityType_Cube);
+
+    Entity::Manager::Instance()->AttachData(entity, data);
+
+    return entity;
+}
 
 using namespace Server;
 
 int main(int argc, char** argv) {
+
+
+
     Start(argc, argv);
 }
