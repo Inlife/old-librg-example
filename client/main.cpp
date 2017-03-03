ba7c1acbd4ac0990161cc7a294df402a04e575c9
Vladislav Gritsenko <vladgritsenko@gmail.com>
2017-03-03 20:04:05 +0200

    addded some stuff from sketchs, not working

--- client/main.cpp
+++ client/main.cpp
@@ -1,5 +1,33 @@
 #include <librg/server.h>
+#include <uv.h>
+#include <unistd.h>
+
+
+/**
+ * Main loop ticker
+ * @param handle [description]
+ */
+void timed_loop(uv_timer_t* handle)
+{
+    printf("hey\n");
+}
+
 
 int main(int argc, char** argv) {
+    uv_timer_t timer_req;
+
+    uv_timer_init(uv_default_loop(), &timer_req);
+    uv_timer_start(&timer_req, timed_loop, 16, 16);
+
+    while (true) {
+        // custom code
+
+        uv_run(uv_default_loop(), UV_RUN_NOWAIT);
+
+
+        // custom code
+        sleep(1);
+    }
+
     return 0;
 }
--- server/main.cpp
+++ server/main.cpp
@@ -13,6 +13,7 @@ using namespace Server;
 enum
 {
     EntityType_Cube = EntityType_FirstFree,
+    EntityType_Vehicle,
     // ...
 };
 
@@ -26,10 +27,57 @@ entity_id create_cube(int res, float size, bool hollow) {
     return entity;
 }
 
-using namespace Server;
 
-int main(int argc, char** argv) {
-    entity_id cube = create_cube(12, 42.3f, false);
-    
-    Start(argc, argv);
+using ResourceManager = Server::Resource::Manager;
+using EntityManager = Server::Resource::Manager;
+
+
+
+struct Vehicle {
+    byte wheels[2];
+    int64_t color[2];
+};
+
+bool test() {
+    return ResourceManager::Instance()->IsRunning("default");
 }
+
+int64_t createVehicle(int model, float x, float y, float z, float rx, float ry, float rz, Function callback) {
+    entity_id entity = Server::Entity::Manager::Create(EntityType_Vehicle);
+
+    return entity;
+}
+
+void sqinstall(Sqrat::Table& native)
+{
+    native.Func("test",  &test);
+    native.Func("createVehicle", &createVehicle);
+}
+
+int main(int argc, char** argv)
+{
+    Server::AddLogger(printer);
+    Server::AddLogger([](int type, std::string message) -> void {
+        printf(message.c_str());
+    });
+
+    Server::SetResourceFolder("resources");
+
+    Server::Scripting::Registry::listeners.insert(sqinstall);
+
+    Server::Start(argc, argv);
+}
+
+// function createVehicle(modelid, x,y,z) {
+//     local entity = null;
+
+//     native.createEntity("type", [], function(entity) {
+//         entity = entity;
+//     });
+
+//     while (!entity) {
+//         // do nothing
+//     }
+
+//     return entity.index;
+// }
