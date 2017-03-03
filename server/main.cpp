2cceb5bfa347e6297582ba646df43270c3778d03
Dominik Madarász <madaraszd@mail.ru>
2017-03-03 22:15:06 +0100

    submodule update

--- server/main.cpp
+++ server/main.cpp
@@ -3,59 +3,6 @@
 
 #include <entityx/entityx.h>
 
-// struct CubeData
-// {
-//     int res;
-//     float wallSize;
-//     bool hollow;
-// };
-
-// using namespace Server;
-
-// enum
-// {
-//     EntityType_Cube = EntityType_FirstFree,
-//     EntityType_Vehicle,
-//     // ...
-// };
-
-// entity_id create_cube(int res, float size, bool hollow) {
-//     auto data = new CubeData {res, size, hollow };
-
-//     auto entity = Entity::Manager::Instance()->Create(EntityType_Cube);
-
-//     Entity::Manager::Instance()->AttachData(entity, data);
-
-//     return entity;
-// }
-
-
-// using ResourceManager = Server::Resource::Manager;
-// using EntityManager = Server::Resource::Manager;
-
-
-
-// struct Vehicle {
-//     byte wheels[2];
-//     int64_t color[2];
-// };
-
-// bool test() {
-//     return ResourceManager::Instance()->IsRunning("default");
-// }
-
-// int64_t createVehicle(int model, float x, float y, float z, float rx, float ry, float rz, Function callback) {
-//     entity_id entity = Server::Entity::Manager::Create(EntityType_Vehicle);
-
-//     return entity;
-// }
-
-// void sqinstall(Sqrat::Table& native)
-// {
-//     native.Func("test",  &test);
-//     native.Func("createVehicle", &createVehicle);
-// }
-
 struct Position {
     Position(float x = 0.0f, float y = 0.0f, float z = 0.0f) : x(x), y(y), z(z) {}
 
@@ -79,8 +26,7 @@ int main(int argc, char** argv)
 
     // Server::Scripting::Registry::listeners.insert(sqinstall);
 
-    entityx::EntityX ex;
-    entityx::Entity entity = ex.entities.create();
+    entityx::Entity entity = Server::Entity::Manager::Instance()->create();
 
     entity.assign<Position>(1.0f, 2.0f, 2.0f);
     entity.assign<Rotation>(1.0f, 2.0f, 2.0f);
