f23effd910f00792ce24f9c6f16275083c415e7f
Dominik Madarasz <madaraszd@mail.ru>
2017-03-06 00:15:18 +0100

    @Added vehicle entity example.

--- server/main.cpp
+++ server/main.cpp
@@ -2,6 +2,8 @@
 #include <librg/Core.h>
 
 #include <entityx/entityx.h>
+#include <entityx/deps/Dependencies.h>
+
 
 struct Position {
     Position(float x = 0.0f, float y = 0.0f, float z = 0.0f) : x(x), y(y), z(z) {}
@@ -15,6 +17,12 @@ struct Rotation {
     float x, y, z;
 };
 
+struct Vehicle {
+    Vehicle(float fuel = 0.0f) : fuel(fuel) {}
+
+    float fuel;
+};
+
 int main(int argc, char** argv)
 {
     // Server::AddLogger(printer);
@@ -26,10 +34,33 @@ int main(int argc, char** argv)
 
     // Server::Scripting::Registry::listeners.insert(sqinstall);
 
+    //NOTE(zaklaus): We can make vehicle dependant on Pos, Rot easily!
+    Server::Entity::Manager::GetSystems()->add<entityx::deps::Dependency<Vehicle, Position, Rotation>>();
+
+    // now let's assume we have native.vehicleCreate(fuel)
+
     entityx::Entity entity = Server::Entity::Manager::Instance()->create();
 
-    entity.assign<Position>(1.0f, 2.0f, 2.0f);
-    entity.assign<Rotation>(1.0f, 2.0f, 2.0f);
+    entity.assign<Vehicle>(80.f);
+
+    // Now, the entity also contains Pos and Rot, we just have to set it using let's say:
+    /*
+        native.movableSetPosition(x,y,z)
+        native.movableSetRotation(x,y,z)
+
+        We can also have native.movableCreate, which would add Position, Rotation and a Scale to the entity, but since Vehicle depends on them, they exist already
+
+        SQ process to create vehicle and move to 120, 40, 8:
+        - local fastcar = native.entityCreate()
+        - native.vehicleCreate(fastcar, 42.0f // fuel //)
+        - native.movableSetPosition(fastcar, [120, 40, 8])
+
+        which can be encapsulated in our SQ framework as:
+        - local fastcar = Vehicle(42.0f, [120, 40, 8])
+        - fastcar.Spawn()
+
+        -- zaklaus
+    */
 
     printf("created entity: %zd\n", entity.id().index());
 
