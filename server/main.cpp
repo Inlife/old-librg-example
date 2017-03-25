9ea313c380cece20603cf2c5ef6e5f33be55f440
Dominik Madarász <madaraszd@mail.ru>
2017-03-25 17:54:58 +0100

    Added streamer remove method.

--- server/main.cpp
+++ server/main.cpp
@@ -1,4 +1,4 @@
-#define RGSERVER
+﻿#define RGSERVER
 
 #include <librg/core/server.h>
 #include <librg/events.h>
@@ -104,6 +104,8 @@ int main(int argc, char** argv)
     // while (true) {
     //     librg::core::client_tick();
     // }
+
+    return 0;
 }
 
 // function createVehicle(modelid, x,y,z) {
